05181e11b30709ac2c2a0a6b79d92ac745e972df
Justin Wood <woody@apple.com>
2017-04-24 12:49:02 -0700

    Simple website to view devices

--- mqtt-state.js
+++ mqtt-state.js
@@ -1,14 +1,41 @@
 // Requirements
 const mqtt = require('mqtt')
+const Redis = require('redis')
+const express = require('express')
 
 const logging = require('./homeautomation-js-lib/logging.js')
-const mqttHelpers = require('./homeautomation-js-lib/mqtt_helpers.js')
 
 // Set up Logging
 logging.set_enabled(true)
 
 // Config
 const host = process.env.MQTT_HOST
+const port = process.env.LISTENING_PORT
+const redisHost = process.env.REDIS_HOST
+const redisPort = process.env.REDIS_PORT
+const redisDB = process.env.REDIS_DATABASE
+
+const redis = Redis.createClient({
+    host: redisHost,
+    port: redisPort,
+    db: redisDB,
+    retry_strategy: function(options) {
+        if (options.error && options.error.code === 'ECONNREFUSED') {
+            // End reconnecting on a specific error and flush all commands with a individual error
+            return new Error('The server refused the connection')
+        }
+        if (options.total_retry_time > 1000 * 60 * 60) {
+            // End reconnecting after a specific timeout and flush all commands with a individual error
+            return new Error('Retry time exhausted')
+        }
+        if (options.times_connected > 10) {
+            // End reconnecting with built in error
+            return undefined
+        }
+        // reconnect after
+        return Math.min(options.attempt * 100, 3000)
+    }
+})
 
 // Setup MQTT
 const client = mqtt.connect(host)
@@ -26,5 +53,63 @@ client.on('disconnect', () => {
 })
 
 client.on('message', (topic, message) => {
-    logging.log(' ' + topic + ':' + message)
+    if (topic.endsWith('/set')) return
+    if (topic.startsWith('/homeseer/action/')) return
+
+    redis.get(topic, function(err, result) {
+        if (result !== null) {
+            // logging.log('already have: ' + topic + ' value: ' + result)
+        } else {
+            logging.log('adding: ' + topic + ' value: ' + message)
+        }
+        redis.set(topic, message)
+    })
+})
+
+// redis callbacks
+
+redis.on('error', function(err) {
+    logging.log('redis error ' + err)
+})
+
+redis.on('connect', function() {
+    logging.log('redis connected')
+    redis.keys('*', function(err, result) {
+        logging.log(' keys:' + result)
+    })
+
+})
+
+// Express
+const app = express()
+
+app.get('/', function(req, res) {
+    redis.keys('*', function(err, result) {
+        logging.log('keys: ' + result)
+        const keys = result.sort()
+        var html = ''
+
+        html += '<!DOCTYPE html>'
+        html += '<html>'
+        html += '<body>'
+        html += '<h1>All Devices</h1>'
+        html += '<table style="width:100%">'
+
+        keys.forEach(function(key) {
+            html += '<tr><td>'
+            html += key
+            html += '</td></tr>'
+
+        }, this)
+
+        html += '</table>'
+        html += '</body>'
+        html += '</html>'
+
+        res.send(html)
+    })
+})
+
+app.listen(port, function() {
+    logging.log('MQTT Store listening on port: ', port)
 })
