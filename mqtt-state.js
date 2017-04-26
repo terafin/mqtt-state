c11f4ec0403696f664abb5ce86cb4a84014b1fe2
Justin Wood <woody@apple.com>
2017-04-26 09:57:19 -0700

    Using new devices

--- mqtt-state.js
+++ mqtt-state.js
@@ -4,6 +4,7 @@ const Redis = require('redis')
 const express = require('express')
 
 const logging = require('./homeautomation-js-lib/logging.js')
+require('./homeautomation-js-lib/devices.js')
 
 // Config
 const host = process.env.MQTT_HOST
@@ -57,20 +58,19 @@ client.on('disconnect', () => {
 })
 
 client.on('message', (topic, message) => {
-    if (topic.endsWith('/set')) return
-    if (topic.startsWith('/homeseer/action/')) return
-
     const components = topic.split('/')
     if (components[0] === 'homeseer' && components[1] === 'action') return
 
-    redis.get(topic, function(err, result) {
+    redis.valueForTopic(topic, function(err, result) {
+        if (err !== null) return
+
         if (result !== null) {
             //logging.log('topic: ' + topic + ' value: ' + result)
             logging.info(JSON.stringify({ topic: topic, value: ('' + message) }))
         } else {
             logging.log('adding: ' + topic + ' value: ' + message)
         }
-        //redis.set(topic, message)
+        redis.set(topic, message)
     })
 })
 
@@ -91,6 +91,7 @@ redis.on('connect', function() {
 // Express
 const app = express()
 
+
 app.get('/', function(req, res) {
     redis.keys('*', function(err, result) {
         logging.log('keys: ' + result)
@@ -98,11 +99,68 @@ app.get('/', function(req, res) {
         redis.mget(keys, function(err, values) {
             var html = ''
 
-            html += '<!DOCTYPE html>'
-            html += '<html>'
-            html += '<body>'
-            html += '<h1>All Devices</h1>'
-            html += '<table style="width:100%">'
+            html += '<!DOCTYPE html>\n'
+            html += '<html>\n'
+            html += '<head>\n'
+
+            html += '<style>'
+            html += '#devices {'
+            html += '    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;'
+            html += '    border-collapse: collapse;'
+            html += '    width: 100%;'
+            html += '}'
+            html += ''
+            html += '#devices td, #customers th {'
+            html += '    border: 1px solid #ddd;'
+            html += '    padding: 8px;'
+            html += '}'
+            html += ''
+            html += '#devices tr:nth-child(even){background-color: #f2f2f2;}'
+            html += ''
+            html += '#devices tr:hover {background-color: #ddd;}'
+            html += ''
+            html += '#devices th {'
+            html += '    padding-top: 12px;'
+            html += '    padding-bottom: 12px;'
+            html += '    text-align: left;'
+            html += '    background-color: #4CAF50;'
+            html += '    color: white;'
+            html += '}'
+            html += '</style>'
+
+            html += '</head>\n'
+            html += '<body>\n'
+            html += '<h1>All Devices</h1>\n'
+            html += '<form action="/action" method="post" form="main_form">'
+            html += '<input type="submit" value="Generate Device File">'
+
+            html += '<table style="width:100%" id="devices">\n'
+
+            html += '<thead>\n'
+            html += '<tr>\n'
+            html += '<th>\n'
+            html += ''
+            html += '</th>\n'
+
+            html += '<th>\n'
+            html += 'Name'
+            html += '</th>\n'
+
+            html += '<th>\n'
+            html += 'Other Topic'
+            html += '</th>\n'
+
+            html += '<th>\n'
+            html += 'Topic'
+            html += '</th>\n'
+            html += '<th>\n'
+            html += 'Value'
+            html += '</th>\n'
+
+            html += '</tr>\n'
+            html += '</thead>\n'
+
+            html += '<tbody>\n'
 
             for (var index = 0; index < keys.length; index++) {
                 var key = keys[index]
@@ -111,20 +169,75 @@ app.get('/', function(req, res) {
                 if (key.endsWith('/set')) continue
                 if (key.startsWith('/homeseer/action/')) continue
 
-                html += '<tr>'
-                html += '<td>'
+                html += '<tr>\n'
+                html += '<td>\n'
+                html += '<input type="checkbox" name="enabled_' + key + '" value="checked" id="enabled" form="main_form"/>'
+                html += '</td>\n'
+
+                html += '<td>\n'
+                html += '<input type="text" name="name_' + key + '" id="name" form="main_form"/>'
+                html += '</td>\n'
+
+                html += '<td>\n'
+                html += '<input type="text" name="topic_' + key + '" id="alttopic" form="main_form"/>'
+                html += '</td>\n'
+
+                html += '<td>\n'
                 html += key
-                html += '</td>'
-                html += '<td>'
+                html += '</td>\n'
+                html += '<td>\n'
                 html += value
-                html += '</td>'
+                html += '</td>\n'
 
-                html += '</tr>'
+                html += '</tr>\n'
             }
+            html += '</tbody>\n'
 
-            html += '</table>'
-            html += '</body>'
+            html += '</table>\n'
+
+            html += '</form>\n'
+
+
+            html += '</body>\n'
             html += '</html>'
+
+            res.send(html)
+        })
+    })
+})
+
+app.get('/device-file/', function(req, res) {
+    redis.keys('*', function(err, result) {
+        if (result === null) {
+            res.send('err' + err)
+            return
+        }
+        logging.log('keys: ' + result)
+        const keys = result.sort()
+        redis.mget(keys, function(err, values) {
+            var html = ''
+            for (var index = 0; index < keys.length; index++) {
+                var key = keys[index]
+
+                if (key.endsWith('/set')) continue
+                if (key.startsWith('/homeseer/action/')) continue
+
+                var components = key.split('/')
+                var lastComponents = components.slice(components.length - 2, components.length)
+                var baseString = lastComponents.join(' ')
+                var name = baseString.replace(/\//g, ' ').replace(/_/g, ' ')
+
+                var title = baseString.replace(/\//g, '_').replace(/ /g, '_')
+                if (title.startsWith('_')) title = title.slice(1, title.length)
+                if (name.startsWith(' ')) name = name.slice(1, name.length)
+                html += '<code>'
+                html += '' + title + ':<br>'
+                html += '&nbsp;&nbsp;name: "' + name + '"<br>'
+                html += '&nbsp;&nbsp;topic: "' + key + '"<br>'
+                html += '&nbsp;&nbsp;change_topic: "' + key + '"<br>'
+                html += '&nbsp;&nbsp;voice_control: false<br>'
+                html += '</code>'
+            }
 
             res.send(html)
         })
