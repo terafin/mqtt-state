e9a46b14ae7496e27f06ab694243193d2f4899f7
Justin Wood <woody@apple.com>
2017-05-10 00:57:57 -0700

    Updating

--- mqtt-state.js
+++ mqtt-state.js
@@ -5,57 +5,16 @@ const express = require('express')
 
 const logging = require('./homeautomation-js-lib/logging.js')
 require('./homeautomation-js-lib/devices.js')
+require('./homeautomation-js-lib/redis_helpers.js')
+require('./homeautomation-js-lib/mqtt_helpers.js')
 
 // Config
-const host = process.env.MQTT_HOST
 const port = process.env.LISTENING_PORT
-const redisHost = process.env.REDIS_HOST
-const redisPort = process.env.REDIS_PORT
-const redisDB = process.env.REDIS_DATABASE
 
-const syslogHost = process.env.SYSLOG_HOST
-const syslogPort = process.env.SYSLOG_PORT
-
-// Set up Logging
-logging.set_enabled(true)
-logging.setRemoteHost(syslogHost, syslogPort)
-
-const redis = Redis.createClient({
-    host: redisHost,
-    port: redisPort,
-    db: redisDB,
-    retry_strategy: function(options) {
-        if (options.error && options.error.code === 'ECONNREFUSED') {
-            // End reconnecting on a specific error and flush all commands with a individual error
-            return new Error('The server refused the connection')
-        }
-        if (options.total_retry_time > 1000 * 60 * 60) {
-            // End reconnecting after a specific timeout and flush all commands with a individual error
-            return new Error('Retry time exhausted')
-        }
-        if (options.times_connected > 10) {
-            // End reconnecting with built in error
-            return undefined
-        }
-        // reconnect after
-        return Math.min(options.attempt * 100, 3000)
-    }
-})
-
-// Setup MQTT
-const client = mqtt.connect(host)
-
-// MQTT Observation
-
-client.on('connect', () => {
-    logging.log('Reconnecting...\n')
+const redis = Redis.setupClient(null)
+const client = mqtt.setupClient(function() {
     client.subscribe('#')
-})
-
-client.on('disconnect', () => {
-    logging.log('Reconnecting...\n')
-    client.connect(host)
-})
+}, null)
 
 client.on('message', (topic, message) => {
     redis.valueForTopic(topic, function(err, result) {
@@ -71,23 +30,8 @@ client.on('message', (topic, message) => {
     })
 })
 
-// redis callbacks
-
-redis.on('error', function(err) {
-    logging.log('redis error ' + err)
-})
-
-redis.on('connect', function() {
-    logging.log('redis connected')
-    redis.keys('*', function(err, result) {
-        logging.log(' keys:' + result)
-    })
-
-})
-
 // Express
 const app = express()
-
 
 app.get('/', function(req, res) {
     redis.keys('*', function(err, result) {
