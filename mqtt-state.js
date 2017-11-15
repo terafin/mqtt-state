b0ce2838e03bf922f5393191a0109029c1be2939
Justin Wood <woody@apple.com>
2017-11-14 20:37:47 -0800

    again

--- mqtt-state.js
+++ mqtt-state.js
@@ -211,8 +211,8 @@ app.get('/device-file/', function(req, res) {
 app.configure(function() {
     app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*')
-        res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE')
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept')
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+        next()
 
         // Pass to next layer of middleware
         next()
