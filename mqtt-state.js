5579b2bfb303111d85f28fd6b556d4b5d4026ddd
Justin Wood <woody@apple.com>
2017-11-14 20:13:45 -0800

    Removing this

--- mqtt-state.js
+++ mqtt-state.js
@@ -207,22 +207,6 @@ app.get('/device-file/', function(req, res) {
     })
 })
 
-// Add headers
-app.use(function(req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001')
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-    // Pass to next layer of middleware
-    next()
-})
-
 app.get('/json/', function(req, res) {
     redis.keys('*', function(err, result) {
         if (result === null) {
