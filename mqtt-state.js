907886d4e95b47f0d7f570f3fb5b6aca5169388b
Justin Wood <woody@apple.com>
2017-11-14 20:29:03 -0800

    Trying again

--- mqtt-state.js
+++ mqtt-state.js
@@ -209,15 +209,9 @@ app.get('/device-file/', function(req, res) {
 
 // Add headers
 app.use(function(req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*')
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept')
 
     // Pass to next layer of middleware
     next()
